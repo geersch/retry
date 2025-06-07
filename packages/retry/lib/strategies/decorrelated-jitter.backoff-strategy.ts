13ee2c7ba589042457b68f0298b3fe989172fc1b
Christophe Geers <geersch@gmail.com>
2025-06-07 13:04:36 +0200

    refactor: variable / constant names for decorrelated backoff strategy

--- packages/retry/lib/strategies/decorrelated-jitter.backoff-strategy.ts
+++ packages/retry/lib/strategies/decorrelated-jitter.backoff-strategy.ts
@@ -1,9 +1,11 @@
 import type { BackoffStrategyConfig } from './backoff-strategy-config.js';
 import type { BackoffStrategy } from './backoff.strategy.js';
 
-const rpScalingFactor = 1 / 1.4;
+const MEDIAN_INTERVAL_SCALING_FACTOR = 1 / 1.4;
 
 /**
+ * Implements decorrelated jitter backoff strategy to prevent thundering herd scenarios.
+ *
  * - https://github.com/App-vNext/Polly/issues/530
  * - https://github.com/Polly-Contrib/Polly.Contrib.WaitAndRetry/blob/24cb116a2a320e82b01f57e13bfeaeff2725ccbf/src/Polly.Contrib.WaitAndRetry/Backoff.DecorrelatedJitterV2.cs
  */
@@ -16,13 +18,13 @@ export class DecorrelatedJitterBackoffStrategy implements BackoffStrategy {
 
   *getGenerator(maxRetries: number): Generator<number> {
     let attempt = 0;
-    let previous = 0;
+    let previousValue = 0;
     while (attempt < maxRetries) {
-      const t = attempt + Math.random();
-      const next = Math.pow(2, t) * Math.tanh(Math.sqrt(4.0 * t));
-      const formulaIntrisincValue = Math.max(0, next - previous);
-      previous = next;
-      yield formulaIntrisincValue * rpScalingFactor * this.baseDelay;
+      const randomizedAttempt = attempt + Math.random();
+      const nextValue = Math.pow(2, randomizedAttempt) * Math.tanh(Math.sqrt(4.0 * randomizedAttempt));
+      const formulaIntrinsicValue = Math.max(0, nextValue - previousValue);
+      previousValue = nextValue;
+      yield formulaIntrinsicValue * MEDIAN_INTERVAL_SCALING_FACTOR * this.baseDelay;
       attempt += 1;
     }
   }
