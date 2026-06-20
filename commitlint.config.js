69b78b1bd0179ad5557843d49962f1efb23ed78d
Christophe Geers <geersch@gmail.com>
2026-06-20 08:37:12 +0200

    commitlint: configure for scoped commits format

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-export default { extends: ['@commitlint/config-conventional'] };
+// Scoped Commits: https://scopedcommits.com
+// Format: <scope>: <description>
+export default {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.+?):\s(.+)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+  rules: {
+    // Allow any scope (disable conventional commits type restrictions)
+    'type-enum': [0],
+    'type-case': [0],
+    // Allow any subject casing
+    'subject-case': [0],
+  },
+};
